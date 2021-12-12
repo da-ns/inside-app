75537c207f4d84699bcdc862bc25958f929ff58e
Natalia Davydova <davydova.n.s@gmail.com>
2021-12-12 12:16:26 +0300

    JWT decoder on client. Message sending.

---
+++ src/main/java/lu/dans/inside24/controllers/Result.java
@@ -0,0 +1,18 @@
+package lu.dans.inside24.controllers;
+
+public class Result {
+
+    private String message;
+
+    public Result(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ src/main/java/lu/dans/inside24/controllers/SentMessage.java
@@ -0,0 +1,24 @@
+package lu.dans.inside24.controllers;
+
+public class SentMessage {
+
+    private String login;
+
+    private String text;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
